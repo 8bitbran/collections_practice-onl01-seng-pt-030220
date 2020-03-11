efca4b7a2d5d4f8dbd0f311248c7c029de96de26
Brandon Marrero <brandonmar97@gmail.com>
2020-03-11 23:22:30 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -20,7 +20,11 @@ def reverse_array(array)
 end 
 
 def kesha_maker(array)
+  new_array = []
   array.each do |string|
-    string[2] = "$"
+    word = string.join()
+    word[2] = "$"
+    new_array << word
   end
+  new_array
 end 
