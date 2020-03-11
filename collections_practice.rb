25370a01785323aec03650a2a9d4bb77c28ecd26
Brandon Marrero <brandonmar97@gmail.com>
2020-03-11 23:44:52 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -39,5 +39,5 @@ def sum_array(array)
 end 
 
 def add_s(array)
-  array.reject.with_index {|string, index| index == 1}.any? {|str| string = string + "s"}
+  array.reject.with_index {|string, index| index == 1}.any? {|str| str = str + "s"}
 end
